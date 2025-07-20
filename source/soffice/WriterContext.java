c7d47291a508f3a7da2bfb387ebb813bd6c4361a
eband <eband@HEESU-4800H.mshome.net>
2025-07-20 11:06:05 +0900

    binData가 없을 경우 null 반환

--- source/soffice/WriterContext.java
+++ source/soffice/WriterContext.java
@@ -393,7 +393,7 @@ public class WriterContext {
             break;
         }
 
-        return binData.format;
+        return binData==null ? null : binData.format;
     }
 
     public static HwpRecord_TabDef getTabDef(short id) {
